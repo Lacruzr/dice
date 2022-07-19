f6f35a6299ab00c133fd2c81c47e8e4e342a0e70
Cindy Navarrete <lacruzr@hotmail.com>
2022-07-18 20:27:28 -0400

    adde efect to the dice rolling usig settimeout

--- dice.js
+++ dice.js
@@ -1,8 +1,30 @@
 
-function generaAleat() {
-    var lance= 0;
+/**
+ const myTimeout = setTimeout(myGreeting, 5000);
+
+function myGreeting() {
+  document.getElementById("demo").innerHTML = "Happy Birthday!"
+}
+ **/
+
+function generaAleat(){
+    const myTimeout1 =  setTimeout(retardo, 300)
+    const myTimeout2 =  setTimeout(retardo, 500)
+    const myTimeout3 =  setTimeout(retardo, 700)
+    const myTimeout4 =  setTimeout(retardo, 900)
+    const myTimeout5 =  setTimeout(retardo, 1100)
+    const myTimeout6 =  setTimeout(retardo, 1300)
+    const myTimeout7 =  setTimeout(retardo, 1500)
+    const myTimeout8 =  setTimeout(retardo, 1800)
+    const myTimeout9 =  setTimeout(retardo, 2100)
+    const myTimeout10 =  setTimeout(retardo, 2400)
     
-        lance= Math.floor(Math.random()*6+1);
+}
+
+
+
+function retardo(){
+       lance= Math.floor(Math.random()*6+1);
         switch(lance){
             case 1:
                 document.getElementById("dado-img").src="./one.png";
